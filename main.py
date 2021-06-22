85d83bb4961ebf8800280f821f63cb8ff057dfc6
rahulraycs <rahsay21039714@gmail.com>
2021-06-22 23:30:59 +0530

    This is a microBlog web app based on python and flask with monogodb as the database .

---
+++ main.py
@@ -0,0 +1,30 @@
+import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+
+
+app = Flask(__name__)
+client = MongoClient("mongodb+srv://rahul:rahul@taskmanager.6prpx.mongodb.net/test")
+app.db = client.microblog
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        app.db.entries.insert({"content": entry_content, "date": formatted_date})
+
+    entries_with_date = [
+        (
+            entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in app.db.entries.find({})
+    ]
+    return render_template("home.html", entries=entries_with_date)
+
+
+if __name__ == "__main__":
+    app.run()
